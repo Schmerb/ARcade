ca4d2feeee126a693df9a781a2fd57c988739da3
Schmerb <mike.schmerbeck@gmail.com>
2018-08-26 10:57:15 -0400

    changed path

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-import store from 'reducers'
+import store from './reducers'
 import App from 'components/app'
 
 import registerServiceWorker from './registerServiceWorker'
